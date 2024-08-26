a1e3dc809679f9d04c2d48c8f7d1e8a78e8a8bd8
github_username <ks05070507@gmail.com>
2024-08-26 18:16:30 +0900

    initial

---
+++ src/test/java/com/rookies2/nagaza/NagazaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookies2.nagaza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NagazaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
