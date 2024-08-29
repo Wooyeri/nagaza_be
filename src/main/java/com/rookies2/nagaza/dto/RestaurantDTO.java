620dfc40ffcd419d1a72fd8b302ba10e281c19bd
Hoyeon <86115801+Hoyeon9@users.noreply.github.com>
2024-08-29 17:27:02 +0900

    Delete scrap things

--- src/main/java/com/rookies2/nagaza/dto/RestaurantDTO.java
+++ src/main/java/com/rookies2/nagaza/dto/RestaurantDTO.java
@@ -1,13 +1,43 @@
 package com.rookies2.nagaza.dto;
 
+import com.rookies2.nagaza.entity.Restaurant;
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-public class RestaurantDTO {
+@NoArgsConstructor
+@AllArgsConstructor
+public class RestaurantDto {
     private Integer id;
     private String name;
     private String posterUrl;
     private String foodType;
     private Integer emotionRating;
     private Integer likeCount;
+
+    // Constructor to map from Restaurant entity to DTO
+    public RestaurantDto(Restaurant restaurant) {
+        if (restaurant != null) {
+            this.id = restaurant.getId();
+            this.name = restaurant.getName();
+            this.posterUrl = restaurant.getPosterUrl();
+            this.foodType = restaurant.getFoodType();
+            this.emotionRating = restaurant.getEmotionRating();
+            this.likeCount = restaurant.getLikeCount();
+        }
+    }
+
+//    public RestaurantDto(Restaurant restaurant) {
+//        RestaurantDto dto = new RestaurantDto();
+//        dto.setId(restaurant.getId());
+//        dto.setName(restaurant.getName());
+//        dto.setPosterUrl(restaurant.getPosterUrl());
+//        dto.setFoodType(restaurant.getFoodType());
+//        dto.setReviewSummary(restaurant.getReviewSummary());
+//        dto.setReviews(restaurant.getReviews());
+//        dto.setEmotionRating(restaurant.getEmotionRating());
+//        dto.setLikeCount(restaurant.getLikeCount());
+//    }
 }
