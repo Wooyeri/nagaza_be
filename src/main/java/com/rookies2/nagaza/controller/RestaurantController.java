2631f5893d2e864cf07552fd9faab1c51eef9904
김재형 <wogud2437@naver.com>
2024-08-30 00:36:26 +0900

    git config core.ignorecase false

--- src/main/java/com/rookies2/nagaza/controller/RestaurantController.java
+++ src/main/java/com/rookies2/nagaza/controller/RestaurantController.java
@@ -1,6 +1,7 @@
 package com.rookies2.nagaza.controller;
 
 import com.rookies2.nagaza.dto.RestaurantDTO;
+import com.rookies2.nagaza.dto.RestaurantDetailDTO;
 import com.rookies2.nagaza.service.RestaurantService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,22 @@ public class RestaurantController {
 
     @Autowired
     private RestaurantService restaurantService;
+
+    /**
+     * 모든 레스토랑을 조회하는 엔드포인트입니다.
+     *
+     * @return 모든 RestaurantDTO의 목록
+     */
+    @GetMapping
+    public List<RestaurantDTO> getAllRestaurants() {
+        return restaurantService.getAllRestaurants();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RestaurantDetailDTO> getDetail(@PathVariable int id) {
+        RestaurantDetailDTO dto = restaurantService.getRestaurantDetail(id);
+        return ResponseEntity.ok(dto);
+    }
 
     @GetMapping("/test/{id}")
     public ResponseEntity<RestaurantDTO> getList(@PathVariable int id){
